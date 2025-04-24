8b7b57caaad203e03b94c1d29a3e623845f07fe6
Kithru Viduranga <kithruV@icbtcampus.local>
2025-04-24 23:48:32 +0530

    Update

--- src/main/java/lk/ijse/spring/WebAppInitializer.java
+++ src/main/java/lk/ijse/spring/WebAppInitializer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 /**
- * @author : Kithru Viduranga
+ * @author : Kithru Viduranga - KV
  * @project : SpringBoot-POS
  **/
 @SpringBootApplication
