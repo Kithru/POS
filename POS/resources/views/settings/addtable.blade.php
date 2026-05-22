9ce6b8da62f9b55c4f17de8a5e68611f070519ab
Kithru Viduranga <kithruV@icbtcampus.local>
2026-05-23 02:02:29 +0530

    Add new changes in manage table blade - button css updates

--- POS/resources/views/settings/addtable.blade.php
+++ POS/resources/views/settings/addtable.blade.php
@@ -2,18 +2,14 @@
 <html lang="en">
 
 <head>
-
     <meta charset="UTF-8">
-
     <title>Manage Tables</title>
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="{{ asset('css/navi.css') }}" rel="stylesheet">
     <link href="{{ asset('css/content.css') }}" rel="stylesheet">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
-
 </head>
 
 <body>
@@ -49,19 +45,18 @@
         <table>
 
             <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Table No</th>
-                    <th>Min Pax</th>
-                    <th>Max Pax</th>
-                    <th>Status</th>
-                    <th>Availability</th>
-                    <th>Action</th>
-                </tr>
+            <tr>
+                <th>#</th>
+                <th>Table No</th>
+                <th>Min Pax</th>
+                <th>Max Pax</th>
+                <th>Status</th>
+                <th>Availability</th>
+                <th>Action</th>
+            </tr>
             </thead>
 
             <tbody>
-
 
             {{-- EXISTING TABLES --}}
             @foreach($tables as $table)
@@ -76,7 +71,7 @@
                         <td>
                             <input type="hidden" name="id" value="{{ $table->id }}">
 
-                            <input class="form-input"
+                            <input class="form-input" style="width:100px;"
                                    type="text"
                                    name="table_number"
                                    value="{{ $table->table_number }}"
@@ -84,7 +79,7 @@
                         </td>
 
                         <td>
-                            <input class="form-input"
+                            <input class="form-input" style="width:100px;"
                                    type="number"
                                    name="min_pax"
                                    value="{{ $table->min_pax }}"
@@ -92,55 +87,43 @@
                         </td>
 
                         <td>
-                            <input class="form-input"
+                            <input class="form-input" style="width:100px;"
                                    type="number"
                                    name="max_pax"
                                    value="{{ $table->max_pax }}"
                                    required>
                         </td>
 
-
-                        {{-- STATUS --}}
+                        {{-- STATUS BUTTON --}}
                         <td>
                             @if($table->table_status == 1)
                                 <a href="{{ route('table.status', $table->id) }}"
-                                   class="status-btn active-btn">
-                                    <i class="fa-solid fa-toggle-on"></i>
-                                    Active
-                                </a>
+                                   class="action-btn edit-btn"
+                                   style="background:#28a745; text-decoration:none;"> Active </a>
                             @else
                                 <a href="{{ route('table.status', $table->id) }}"
-                                   class="status-btn inactive-btn">
-                                    <i class="fa-solid fa-toggle-off"></i>
-                                    Inactive
-                                </a>
+                                   class="action-btn delete-btn"
+                                   style="background:#dc3545; text-decoration:none;"> Inactive </a>
                             @endif
                         </td>
 
-
-                        {{-- AVAILABILITY --}}
+                        {{-- AVAILABILITY BUTTON --}}
                         <td>
                             @if($table->availability == 1)
                                 <a href="{{ route('table.availability', $table->id) }}"
-                                   class="status-btn available-btn">
-                                    <i class="fa-solid fa-circle-check"></i>
-                                    Available
-                                </a>
+                                   class="action-btn edit-btn"
+                                   style="background:#17a2b8; text-decoration:none;"> Available </a>
                             @else
                                 <a href="{{ route('table.availability', $table->id) }}"
-                                   class="status-btn unavailable-btn">
-                                    <i class="fa-solid fa-circle-xmark"></i>
-                                    Unavailable
-                                </a>
+                                   class="action-btn delete-btn"
+                                   style="background:#6c757d; text-decoration:none;"> Unavailable </a>
                             @endif
                         </td>
 
-
-                        {{-- ACTION (ONLY UPDATE) --}}
+                        {{-- ACTION --}}
                         <td>
                             <button type="submit" class="action-btn edit-btn">
-                                <i class="fa-solid fa-pen"></i>
-                                Update
+                                <i class="fa-solid fa-pen"></i> Update
                             </button>
                         </td>
 
@@ -160,7 +143,7 @@
                     <td>New</td>
 
                     <td>
-                        <input class="form-input"
+                        <input class="form-input" style="width:100px;"
                                type="text"
                                name="table_number"
                                placeholder="Table Number"
@@ -168,7 +151,7 @@
                     </td>
 
                     <td>
-                        <input class="form-input"
+                        <input class="form-input" style="width:100px;"
                                type="number"
                                name="min_pax"
                                placeholder="Min Pax"
@@ -176,31 +159,20 @@
                     </td>
 
                     <td>
-                        <input class="form-input"
+                        <input class="form-input" style="width:100px;"
                                type="number"
                                name="max_pax"
                                placeholder="Max Pax"
                                required>
                     </td>
 
-                    <td>
-                        <span class="status-btn active-btn">
-                            <i class="fa-solid fa-toggle-on"></i>
-                            Active
-                        </span>
-                    </td>
-
-                    <td>
-                        <span class="status-btn available-btn">
-                            <i class="fa-solid fa-circle-check"></i>
-                            Available
-                        </span>
-                    </td>
+                    {{-- EMPTY LIKE YOUR PREFECTURE STYLE --}}
+                    <td></td>
+                    <td></td>
 
                     <td>
                         <button type="submit" class="action-btn edit-btn">
-                            <i class="fa-solid fa-plus"></i>
-                            Add Table
+                            <i class="fa-solid fa-plus"></i> Add
                         </button>
                     </td>
 
