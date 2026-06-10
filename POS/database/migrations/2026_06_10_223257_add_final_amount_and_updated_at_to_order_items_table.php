d059e538945bffe286b7ad757d247618e0d1fe7a
Kithru Viduranga <kithruV@icbtcampus.local>
2026-06-10 22:59:21 +0530

    Add two columns to order items table with migrations

---
+++ POS/database/migrations/2026_06_10_223257_add_final_amount_and_updated_at_to_order_items_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void{
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->decimal('final_amount', 10, 2)
+                  ->default(0)
+                  ->after('subtotal');
+
+            $table->timestamp('updated_at')
+                  ->nullable()
+                  ->after('final_amount');
+        });
+    }
+
+    public function down(): void {
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->dropColumn([
+                'final_amount',
+                'updated_at'
+            ]);
+        });
+    }
+};
